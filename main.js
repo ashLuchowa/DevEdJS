af551c6bdb06db1f44eb39c810be2e4e30bfe9e3
Ash Luchowa <ashwinluchowa@gmail.com>
2023-02-08 12:35:49 +0800

    Add event listener to close button

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const modalBtn = document.querySelector(".modal-btn");
 const modalBg = document.querySelector(".modal-bg");
+const modalCloneBtn = document.querySelector(".modal-close");
 
 modalBtn.addEventListener("click", () => {
   modalBg.classList.add("bg-active");
+});
+
+modalCloneBtn.addEventListener("click", () => {
+  modalBg.classList.remove("bg-active");
 });
