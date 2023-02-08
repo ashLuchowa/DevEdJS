9ae93333522a1f3892afe43d5d0e998ae853fa8c
Ash Luchowa <ashwinluchowa@gmail.com>
2023-02-08 14:42:24 +0800

    Modify slider to start on img1

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
 let counter = 1;
 const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = "translate(" + -size * counter + "px)";
