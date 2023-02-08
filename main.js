4fae182af3d508f0305fb1f8f53d8a808f38c936
Ash Luchowa <ashwinluchowa@gmail.com>
2023-02-08 15:07:12 +0800

    Fix clicking too fast overflow bug

--- main.js
+++ main.js
@@ -8,12 +8,14 @@ const size = carouselImages[0].clientWidth;
 carouselSlide.style.transform = "translate(" + -(size * counter) + "px)";
 
 nextBtn.addEventListener("click", () => {
+  if (counter >= carouselImages.length - 1) return;
   carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter++;
   carouselSlide.style.transform = "translate(" + -(size * counter) + "px)";
 });
 
 prevBtn.addEventListener("click", () => {
+  if (counter <= 0) return;
   carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter--;
   carouselSlide.style.transform = "translate(" + -(size * counter) + "px)";
